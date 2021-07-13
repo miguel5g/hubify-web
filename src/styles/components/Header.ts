42b0e68d76fa2cf43bbe3f2df72ed78038f8748f
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-13 02:33:13 -0300

    chore: Alterado position do Header para sticky

--- src/styles/components/Header.ts
+++ src/styles/components/Header.ts
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
+  position: sticky;
+  top: 0px;
+
   display: flex;
   align-items: center;
   justify-content: center;
