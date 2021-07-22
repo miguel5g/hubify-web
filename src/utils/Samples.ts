13ff6f7d30c00325eefaa6053ccb4346e33b18f5
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-22 11:30:27 -0300

    chore: Arquivo com exemplos enquanto não tem api

---
+++ src/utils/Samples.ts
@@ -0,0 +1,21 @@
+export const appFeedbackData: AppFeedbackData = {
+  id: '1',
+  author: 'Miguel Ângelo',
+  author_id: '1',
+  image_url:
+    'https://media.discordapp.net/attachments/796091203082649640/867481825018249216/photo-1494790108377-be9c29b29330.png?width=315&height=473',
+  title: 'Titulo do feedback',
+  content:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi facere eius maxime aspernatur dolor numquam, adipisci debitis assumenda omnis praesentium nesciunt sequi, quo obcaecati nisi dolores reprehenderit neque expedita. Omnis!',
+};
+
+export const projectData: ProjectData = {
+  id: '1',
+  author: 'Miguel Ângelo',
+  author_id: '1',
+  title: 'Social Network',
+  description: 'Um simples projeto para desenvolver abilidaes',
+  stars: 113,
+  feedbacks: 9,
+  impulses: 38,
+};
