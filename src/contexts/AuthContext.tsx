ff575428e7bcaf62061452b47f650dfe89be0c7f
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-29 15:08:58 -0300

    feat: Conectado com a API

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,11 +1,15 @@
+import { AxiosError } from 'axios';
 import React, { createContext } from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+
+import { api } from '../services/Api';
 
 interface AuthContextType {
   user?: UserData;
   isAuth: boolean;
   refresh: () => Promise<void>;
+  logOut: () => Promise<void>;
 }
 
 export const AuthContext = createContext({} as AuthContextType);
@@ -13,12 +17,46 @@ export const AuthContext = createContext({} as AuthContextType);
 export const AuthContextProvider: React.FC = ({ children }) => {
   const [user, setUser] = useState<UserData>();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const fromSessionStorage = sessionStorage.getItem('user-session');
 
-  async function refresh() {}
+    if (fromSessionStorage) {
+      setUser(JSON.parse(fromSessionStorage));
+      refresh();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (user) sessionStorage.setItem('user-session', JSON.stringify(user));
+  }, [user]);
+
+  async function refresh() {
+    api
+      .get('/user')
+      .then(({ data }) => {
+        setUser({
+          id: data.id,
+          username: data.username,
+          avatar_url: data.avatar_url,
+        });
+      })
+      .catch(({ response, ...err }: AxiosError) => {
+        if (response) {
+          if (response.status === 401) logOut();
+        }
+      });
+  }
+
+  async function logOut() {
+    api.delete('/session');
+    setUser(undefined);
+    sessionStorage.removeItem('user-session');
+  }
 
   return (
-    <AuthContext.Provider value={{ user, isAuth: user !== undefined, refresh }}>
+    <AuthContext.Provider
+      value={{ user, isAuth: user !== undefined, refresh, logOut }}
+    >
       {children}
     </AuthContext.Provider>
   );
