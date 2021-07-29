de0abd1f6b2fd3ca066af4ede10ac65de4f726fa
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-29 14:52:44 -0300

    feat: Adicionado funcionabilidade de criar conta

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head';
+import { AxiosError } from 'axios';
 import { useRouter } from 'next/router';
 import { toast } from 'react-hot-toast';
 import { FiLock, FiMail, FiUser } from 'react-icons/fi';
+
+import { api } from '../services/Api';
 
 import { Anchor } from '../components/Anchor';
 import { Button } from '../components/Button';
@@ -10,13 +13,55 @@ import { Button } from '../components/Button';
 import { AuthContainer } from '../styles/pages/Auth';
 
 const SignUp: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
   const router = useRouter();
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    if (isLoading) return;
+
     e.preventDefault();
-    toast('Infelizmente isso ainda não funciona...', {
-      icon: '🙁',
+
+    setLoading(true);
+
+    const signUpPromise = new Promise<void>((resolve, reject) => {
+      api
+        .post('/user', {
+          username,
+          email,
+          password,
+        })
+        .then(() => {
+          setLoading(false);
+          resolve();
+        })
+        .catch(({ response, ...err }: AxiosError) => {
+          setLoading(false);
+
+          if (response) {
+            reject(
+              response.data.errors[0] ||
+                response.data.error ||
+                response.data.message
+            );
+          }
+
+          reject('Erro inesperado...');
+        });
     });
+
+    toast
+      .promise(signUpPromise, {
+        loading: 'Aguarde...',
+        success: 'Faça login para terminar seu cadastro.',
+        error: (err: string) => err,
+      })
+      .then(async () => {
+        router.push('/signin');
+      });
   }
 
   return (
@@ -33,7 +78,12 @@ const SignUp: React.FC = () => {
             <FiUser />
             Usuário
           </label>
-          <input type="text" name="username" id="username" />
+          <input
+            type="text"
+            name="username"
+            id="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
 
         <div>
@@ -41,7 +91,12 @@ const SignUp: React.FC = () => {
             <FiMail />
             E-mail
           </label>
-          <input type="email" name="email" id="email" />
+          <input
+            type="email"
+            name="email"
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
 
         <div>
@@ -49,12 +104,17 @@ const SignUp: React.FC = () => {
             <FiLock />
             Senha
           </label>
-          <input type="password" name="password" id="password" />
+          <input
+            type="password"
+            name="password"
+            id="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
 
         <div className="button-group">
           <Button onClick={() => router.back()}>Voltar</Button>
-          <Button isPrimary type="submit">
+          <Button isPrimary type="submit" disabled={isLoading}>
             Registrar
           </Button>
         </div>
