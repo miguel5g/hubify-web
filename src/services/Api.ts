4d1b62d7d4bd756467b8b5dfb8cf713b1c81af64
Miguel Ângelo <miguelcg958@gmail.com>
2021-07-29 18:11:21 -0300

    chore: Alterado a url da api

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -4,14 +4,14 @@ let baseURL: string;
 
 if (process.browser)
   baseURL =
-    window.location.origin === 'http://localhost:3000'
-      ? 'http://localhost:3333'
+    window.location.origin === 'http://192.168.0.100:3000'
+      ? 'http://192.168.0.100:3333'
       : 'https://hubify-api.herokuapp.com';
 else
   baseURL =
     process.env.NODE_ENV === 'production'
       ? 'https://hubify-api.herokuapp.com'
-      : 'http://localhost:3333';
+      : 'http://192.168.0.100:3333';
 
 export const api = axios.create({
   baseURL,
